7dcae1a69de820cfb56e9f943c0bfce8e68eba4d
Nemanja Milic <milicn18@gmail.com>
2020-10-14 21:54:12 +0200

    ubacen db.php fajl

--- posts.php
+++ posts.php
@@ -1,20 +1,5 @@
-<?php
-    $servername = "127.0.0.1";
-    $username = "root";
-    $password = "vivify";
-    $dbname = "blog";
-
-    try {
-        $connection = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-        // set the PDO error mode to exception
-        $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    }
-    catch(PDOException $e)
-    {
-        echo $e->getMessage();
-    }
+<?php include('db.php');
 ?>
-
 
 <?php
                 $sql1 ="SELECT *  FROM posts ORDER BY created_at DESC";
